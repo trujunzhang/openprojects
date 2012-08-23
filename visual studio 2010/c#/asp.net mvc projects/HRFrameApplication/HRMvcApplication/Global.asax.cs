a2365533731a8230af15a8e0de9581bafe15fc19
wanghaogithub720 <wanghaomspace@gmail.com>
2012-08-23 17:22:43 +0800

    add MainPageController's Index

--- visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Global.asax.cs
+++ visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Global.asax.cs
@@ -24,7 +24,7 @@ namespace HRMvcApplication
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "MainPage", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
