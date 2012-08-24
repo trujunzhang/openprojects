b2279b5dc3c4fdd5156fce8abcbd89305aba5d9c
wanghaogithub720 <wanghaomspace@gmail.com>
2012-08-24 09:45:48 +0800

    Mechanism/List by MvcContrib.dll

---
+++ visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Controllers/MechanismController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HRMvcApplication.Controllers
+{
+    public class MechanismController : Controller
+    {
+        //
+        // GET: /Mechanism/
+
+        public ActionResult List(string searchWord, int? genreId, int? artistId, GridSortOptions gridSortOptions,
+                                 int? page)
+        {
+            PagedViewModel<C_Mechanism> pagedViewModel = new PagedViewModel<C_Mechanism>
+                                                             {
+                                                                 ViewData = ViewData,
+                                                                 Query = new BLL.C_Mechanism().GetList().AsQueryable(),
+                                                                 GridSortOptions = gridSortOptions,
+                                                                 DefaultSortColumn = "MECHANISMID",
+                                                                 Page = page,
+                                                                 PageSize = 10,
+                                                             }
+                .AddFilter("searchWord", searchWord, a => a.机构名称.Contains(searchWord))
+                .Setup();
+
+            return View(pagedViewModel);
+        }
+
+        }
+
+    }
+}
---
+++ visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Models/PagedViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web.Mvc;
+
+namespace HRMvcApplication.Models
+{
+    public class PagedViewModel<T>
+    {
+        public ViewDataDictionary ViewData { get; set; }
+        public IQueryable<T> Query { get; set; }
+        public GridSortOptions GridSortOptions { get; set; }
+        public string DefaultSortColumn { get; set; }
+        public IPagination<T> PagedList { get; private set; }
+        public int? Page { get; set; }
+        public int? PageSize { get; set; }
+
+        public PagedViewModel<T> AddFilter(Expression<Func<T, bool>> predicate)
+        {
+            Query = Query.Where(predicate);
+            return this;
+        }
+
+        public PagedViewModel<T> AddFilter<TValue>(string key, TValue value, Expression<Func<T, bool>> predicate)
+        {
+            ProcessQuery(value, predicate);
+            ViewData[key] = value;
+            return this;
+        }
+
+        public PagedViewModel<T> AddFilter<TValue>(string keyField, object value, Expression<Func<T, bool>> predicate,
+            IQueryable<TValue> query, string textField)
+        {
+            ProcessQuery(value, predicate);
+            var selectList = query.ToSelectList(keyField, textField, value);
+            ViewData[keyField] = selectList;
+            return this;
+        }
+
+        public PagedViewModel<T> Setup()
+        {
+            if (string.IsNullOrWhiteSpace(GridSortOptions.Column))
+            {
+                GridSortOptions.Column = DefaultSortColumn;
+            }
+
+            PagedList = Query.OrderBy(GridSortOptions.Column, GridSortOptions.Direction)
+                .AsPagination(Page ?? 1, PageSize ?? 10);
+
+            return this;
+        }
+
+        private void ProcessQuery<TValue>(TValue value, Expression<Func<T, bool>> predicate)
+        {
+            if (value == null) return;
+            if (typeof(TValue) == typeof(string))
+            {
+                if (string.IsNullOrWhiteSpace(value as string)) return;
+            }
+
+            Query = Query.Where(predicate);
+        }
+    }
+}
