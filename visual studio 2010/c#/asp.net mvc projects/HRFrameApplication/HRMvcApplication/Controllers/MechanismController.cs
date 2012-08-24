dd4918cd38854ee392eb03b0eba4281a337bde92
wanghaogithub720 <wanghaomspace@gmail.com>
2012-08-24 14:14:26 +0800

    fix Details.cshtml in MechanismController.cs

--- visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Controllers/MechanismController.cs
+++ visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Controllers/MechanismController.cs
@@ -8,8 +8,7 @@ namespace HRMvcApplication.Controllers
 {
     public class MechanismController : Controller
     {
-        //
-        // GET: /Mechanism/
+        #region List view
 
         public ActionResult List(string searchWord, int? genreId, int? artistId, GridSortOptions gridSortOptions,
                                  int? page)
@@ -28,5 +27,51 @@ namespace HRMvcApplication.Controllers
 
             return View(pagedViewModel);
         }
+
+        #endregion
+
+        #region detail view
+
+        public ActionResult Details(int id)
+        {
+            //id = 28;
+            var viewModel = new MechanismDetailModel(new BLL.C_Mechanism().GetModel(id));
+            Session["mechanismid"] = id;
+            Session["viewmodel"] = viewModel; // used by MechanismModel()
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Details(MechanismDetailModel model, string saveto, string remove, string up, string down)
+        {
+            var viewModel = Session["viewmodel"] as MechanismDetailModel; // used by MechanismModel()
+            if (!string.IsNullOrEmpty(saveto)) //model的form事件【保存或更新】
+            {
+                model.SelectedModel.删除标识 = false;
+                new BLL.C_Mechanism().Update(model.SelectedModel);
+            }
+            else if (!string.IsNullOrEmpty(remove)) //联系人列表删除事件
+            {
+                //if (model.RequestedProducts != null)
+                if (true)
+                {
+                    //foreach (int PERSONINCHARGEID in model.RequestedProducts)
+                    if (true)
+                    {
+                        //C_PersonInCharge personInCharge = new BLL.C_PersonInCharge().GetModel(PERSONINCHARGEID);
+                        //personInCharge.删除标识 = true;
+                        //new BLL.C_PersonInCharge().Update(personInCharge);
+                    }
+                }
+            }
+            else // 弹出联系人对话框后，调用this.form.subform后，重新更新联系人列表的事件
+            {
+                //model.SelectedMechanismid = (int) model.SelectedModel.MECHANISMID;
+            }
+
+            return View(viewModel);
+        }
+
+        #endregion
     }
 }
---
+++ visual studio 2010/c#/asp.net mvc projects/HRFrameApplication/HRMvcApplication/Models/MechanismDetailModel.cs
@@ -0,0 +1,22 @@
+﻿using Model;
+
+namespace HRMvcApplication.Models
+{
+    public class MechanismDetailModel
+    {
+        #region class fields 
+
+        public C_Mechanism SelectedModel { get; set; }
+
+        #endregion
+
+        #region constructor
+
+        public MechanismDetailModel(C_Mechanism cMechanism)
+        {
+            SelectedModel = cMechanism;
+        }
+
+        #endregion
+    }
+}
